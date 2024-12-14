b65f2ff582abe73fc59e687c6b1baa3122310dec
Dmitriy Butenko <dimas.ukimas@gmail.com>
2024-12-14 21:06:07 +0300

    feat(util): add HibernateUtil class

---
+++ src/main/java/com/dimasukimas/tennisscoreboard/util/HibernateUtil.java
@@ -0,0 +1,25 @@
+package com.dimasukimas.tennisscoreboard.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static Session getSession() {
+        return sessionFactory.openSession();
+    }
+
+
+
+}
