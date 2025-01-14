6bf059092a121103829ba7cc8844bfcb54919f84
Dmitriy Butenko <dimas.ukimas@gmail.com>
2025-01-14 18:35:38 +0300

    refactor: change HibernateUtil to provide SessionFactory instead Session

--- src/main/java/com/dimasukimas/tennisscoreboard/util/HibernateUtil.java
+++ src/main/java/com/dimasukimas/tennisscoreboard/util/HibernateUtil.java
@@ -1,25 +1,25 @@
 package com.dimasukimas.tennisscoreboard.util;
 
-import org.hibernate.Session;
+import com.dimasukimas.tennisscoreboard.model.FinishedMatch;
+import com.dimasukimas.tennisscoreboard.model.Player;
+import lombok.Getter;
+import lombok.experimental.UtilityClass;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+@UtilityClass
 public class HibernateUtil {
 
+    @Getter
     private static final SessionFactory sessionFactory;
 
     static {
 
         Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(Player.class);
+        configuration.addAnnotatedClass(FinishedMatch.class);
         configuration.configure();
 
         sessionFactory = configuration.buildSessionFactory();
     }
-
-    public static Session getSession() {
-        return sessionFactory.openSession();
-    }
-
-
-
 }
