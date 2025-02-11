d06aea3601e4e58144009792099c5cc2dbdd058b
Dmitriy Butenko <dimas.ukimas@gmail.com>
2025-02-11 18:57:08 +0300

    refactor(controller): change MatchScoreController dependencies

--- src/main/java/com/dimasukimas/tennisscoreboard/controller/MatchScoreController.java
+++ src/main/java/com/dimasukimas/tennisscoreboard/controller/MatchScoreController.java
@@ -1,33 +1,22 @@
 package com.dimasukimas.tennisscoreboard.controller;
 
-import com.dimasukimas.tennisscoreboard.dto.MatchScoreResponseDto;
-import com.dimasukimas.tennisscoreboard.model.match.OngoingMatch;
-import com.dimasukimas.tennisscoreboard.repository.MatchRepository;
-import com.dimasukimas.tennisscoreboard.repository.PlayerRepository;
-import com.dimasukimas.tennisscoreboard.service.FinishedMatchesPersistenceService;
+import com.dimasukimas.tennisscoreboard.model.dto.MatchScoreResponseDto;
 import com.dimasukimas.tennisscoreboard.service.MatchScoreCalculationService;
 import com.dimasukimas.tennisscoreboard.service.OngoingMatchesService;
-import com.dimasukimas.tennisscoreboard.util.HibernateUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.hibernate.SessionFactory;
 
 import java.io.IOException;
-import java.util.Optional;
 import java.util.UUID;
 
 @WebServlet("/match-score")
 public class MatchScoreController extends HttpServlet {
 
-    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-    PlayerRepository playerRepository = new PlayerRepository();
-    MatchRepository matchRepository = MatchRepository.getInstance();
-    private final OngoingMatchesService ongoingMatchesService = OngoingMatchesService.getInstance (playerRepository , sessionFactory);
-    private final FinishedMatchesPersistenceService finishedMatchesPersistenceService = FinishedMatchesPersistenceService.getInstance(sessionFactory, matchRepository);
-    private final MatchScoreCalculationService matchScoreCalculationService = MatchScoreCalculationService.getInstance(ongoingMatchesService, finishedMatchesPersistenceService);
+    private final OngoingMatchesService ongoingMatchesService = OngoingMatchesService.getInstance ();
+    private final MatchScoreCalculationService matchScoreCalculationService = MatchScoreCalculationService.getInstance();
 
 public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
